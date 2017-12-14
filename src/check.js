f705287f49cce1405e21c082d141c3c328121e44
g.lariviere <g.lariviere@qwant.com>
2017-12-14 17:00:07 +0100

    [FIX] check name type

--- src/check.js
+++ src/check.js
@@ -4,18 +4,18 @@ const { exitWithMessage } = require('./errorHandler')
 
 exports.checkProgramArguments = (name, destination) => {
   // Check if name params is undefined
-  if (!name)
-    exitWithMessage('Component name is a required parameter (pop --help)')
+  if (typeof name !== 'string')
+    return exitWithMessage('Component name is a required parameter (pop --help)')
 
   // Check if Component name start with an uppercase letter
   if (name[0] !== name[0].toUpperCase())
-    exitWithMessage('Component name should start with a capital letter')
+    return exitWithMessage('Component name should start with a capital letter')
 
   // Check if destination exists
   if (!fs.existsSync(destination))
-    exitWithMessage(`Location ${destination} does not exists`)
+    return exitWithMessage(`Location ${destination} does not exists`)
 
   // Check if component folder is available
   if (fs.existsSync(`${destination}/${name}`))
-    exitWithMessage(`File ${name} already exists at ${destination}`)
+    return exitWithMessage(`File ${name} already exists at ${destination}`)
 }
